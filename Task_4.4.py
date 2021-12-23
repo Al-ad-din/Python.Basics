320a9cb09396b6e7976a2b8fff3bee5750da4918
Al-ad-din <al-ad-din@yandex.ru>
2021-12-23 16:18:07 +0300

    Практическое задание к уроку № 4

---
+++ Task_4.4.py
@@ -0,0 +1,6 @@
+import currency_rates
+
+print('USD', *currency_rates.currency_rates('usd'))
+print('EUR', *currency_rates.currency_rates('eur'))
+print('AUD', *currency_rates.currency_rates('aud'))
+print('BYN', *currency_rates.currency_rates('byn'))
