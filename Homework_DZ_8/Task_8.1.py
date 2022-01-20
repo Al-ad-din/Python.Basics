add8f16e0a7edcd35cbe6a279e5f04a312be99bc
Al-ad-din <al-ad-din@yandex.ru>
2022-01-20 21:13:26 +0300

    Практическое задание к уроку № 8

---
+++ Homework_DZ_8/Task_8.1.py
@@ -0,0 +1,10 @@
+def email_parse(email):
+    import re
+    check = re.match(r'\w+\@\w+\.\w+', email)
+    if check:
+        pars = re.split(r'@', email)
+        print({'username': pars[0], 'domain': pars[1]})
+    else:
+        raise ValueError(f'wrong email: {email}')
+
+email_parse('2064090@mail.ru')
